91986b0ab125872b1b0542d78d03bd313cea5634
kent fueconcillo <kentoyfueconcillo@gmail.com>
2017-10-21 15:12:59 +0800

    https://whoyou.herokuapp.com

--- whoyouproject/settings.py
+++ whoyouproject/settings.py
@@ -177,7 +177,7 @@ CORS_ORIGIN_WHITELIST = (
     'localhost:4200',
     'localhost:8000',
     'localhost:6379',
-    'whoyou.herokuapp.com'
+    'https://whoyou.herokuapp.com'
 )
 
 CORS_ALLOW_METHODS = (
