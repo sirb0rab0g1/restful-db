d6557a6a923fa7d82f6ba30eb84507d44822dbf9
kent fueconcillo <kentoyfueconcillo@gmail.com>
2017-10-21 15:51:54 +0800

    lol

--- whoyouproject/settings.py
+++ whoyouproject/settings.py
@@ -160,8 +160,8 @@ MEDIA_URL = '/media/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
